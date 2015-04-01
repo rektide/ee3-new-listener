c09141b73b6fa8d89d8706616724c343b351ec75
rektide <rektide@voodoowarez.com>
2015-04-01 01:59:45 -0400

    Use Mix-Master utility to create wrapper slots.

--- ee3-newListener.js
+++ ee3-newListener.js
@@ -1,15 +1,46 @@
+var mixMaster = require('mix-master')
+
 module.exports= function(eventEmitter){
-	function once(eventName, listener){
-		this._once(eventName, listener)
-		this.emit('newListener', eventName, listener)
+	function emittingOn(name, listener){
+		arguments.callee._get.call(this).call(this, name, listener)
+		this.emit('newListener', name, listener)
 	}
-	var _once = eventEmitter.once
-	Object.defineProperty(eventEmitter, '_once', {
-		value: _once
-		writable: true
-	})
-	Object.defineProperty(eventEmitter, 'once', {
-		value: once,
-		writable: true
-	})
+	mixMaster(eventEmitter, 'on', emittingOn)
+	mixMaster(eventEmitter, 'addListener', emittingOn)
+
+	function emittingOnce(name, listener){
+		arguments.callee._get.call(this).call(this, name, listener)
+		this.emit('newListener', name, listener)
+	}
+	mixMaster(eventEmitter, 'once', emittingOnce)
+
+	function emittingRemoveListener(name, listener){
+		var previous = this._events[name]
+		arguments.callee._get.call(this).call(this, name, listener)
+		if(this._events[name] !== previous){
+			this.emit('removeListener', name, listener)
+		}
+	}
+	mixMaster(eventEmitter, 'removeListener', emittingRemoveListener)
+
+	function emittingRemoveAllListeners(name){
+		var removeListeners = this.listeners('removeListener')
+		if(removeListeners){
+			if(name === undefined){
+				for(var i in this._events){
+					var these= this._events[these]
+					for(var j in these){
+						this.emit('removeListener', i, these[j].fn)
+					}
+				}
+			}else{
+				var these= this.listeners(name)
+				for(var j in these){
+					this.emit('removeListener', name, these[j].fn)
+				}
+			}
+		}
+		arguments.callee._get.call(this).call(this, name, listener)
+	}
+	mixMaster(eventEmitter, 'removeAllListeners', emittingRemoveAllListeners)
 }
